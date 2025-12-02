b3ec0344016e2871c1f96e8a637c2e49c1ec7d3e
Aditya101415 <adityayadav01415@gmail.com>
2025-12-02 12:57:00 +0530

    ..

---
+++ Web Devlopment/2nd dec/app.js
@@ -0,0 +1,14 @@
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+console.log(NaN/2);
+let s='JavaScript';
+s[0]='K';
+console.log(s);
+let num=0o71;
+console.log(num);
+
+
+document.getElementById("myBtn").addEventListener("click", function() {
+    document.body.innerHTML += "<p>You clicked the button!</p>";
+}); 
+
